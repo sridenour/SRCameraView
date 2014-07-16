92d52bbd8802b0ae76ada0e6b1d4c2dedd4b1f32
Tancrède Chazallet (AncAinu) <contact@ancainu.com>
2014-07-16 11:02:32 +0200

    Added copyright

--- source/SRCameraView.m
+++ source/SRCameraView.m
@@ -2,6 +2,7 @@
 //  SRCameraView.m
 /*
  Copyright (c) 2012, 2013 Sean Ridenour
+ Copyright (c) 2014, Sean Ridenour, Tancrède Chazallet
  
  Permission is hereby granted, free of charge, to any person obtaining a copy of
  this software and associated documentation files (the "Software"), to deal in
